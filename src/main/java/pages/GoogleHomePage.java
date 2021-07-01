63aa8f4d87667ee2532fcf6fa35a9ce099402348
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2021-06-30 20:17:19 -0400

    Update challenge 1, to add some functionality that the Python WebDriver certification adds

--- src/main/java/pages/GoogleHomePage.java
+++ src/main/java/pages/GoogleHomePage.java
@@ -1,18 +1,20 @@
 package pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class GoogleHomePage {
 
-    private final WebDriver driver;
-
     private final static String url = "https://www.google.com/";
+    private final WebDriver driver;
 
     public GoogleHomePage(WebDriver driver) {
         this.driver = driver;
         driver.get(url);
+    }
+
+    public void enterSearchKey(String searchKey) {
+        driver.findElement(By.xpath("//input[@name='q']")).sendKeys(searchKey, Keys.ENTER);
     }
 }
--- src/test/java/challenges/challenge1.java
+++ src/test/java/challenges/challenge1.java
@@ -5,14 +5,18 @@ import org.testng.annotations.Test;
 
 public class challenge1 extends BaseTests {
 
+    private final static String searchKey = "puppies";
+
     @Test
     public void testGoogleTitle() {
         goToGoogle();
         assertTitleAsExpected("Google");
 
-        // TODO: The Python WebDriver certification also has:
-        //      1. Search for “puppies”
-        //      2. Assert that the results page that loads has “puppies” in its title
-        // So, for comparison between Java and Python, implement that as well.
+        // The Python WebDriver certification also includes 2 additional requirements:
+        // • Search for “puppies”
+        // • Assert that the results page that loads has “puppies” in its title
+        // So, for comparison between Java and Python, that has been implemented below.
+        googleHomePage.enterSearchKey(searchKey);
+        assertTitleContains(searchKey);
     }
 }
